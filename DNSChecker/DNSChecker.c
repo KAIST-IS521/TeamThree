877b40601fa0fed2bb579b75b201d28cabcbef2e
seongil-wi <su3604@kaist.ac.kr>
2017-05-16 19:40:06 +0900

    Add the material of RAW DNS Packet

--- DNSChecker/DNSChecker.c
+++ DNSChecker/DNSChecker.c
@@ -8,13 +8,73 @@
  * */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+//DNS header structure
+struct DNS_HEADER
+{
+    unsigned short id; // identification number
+ 
+    unsigned char rd :1; // recursion desired
+    unsigned char tc :1; // truncated message
+    unsigned char aa :1; // authoritive answer
+    unsigned char opcode :4; // purpose of message
+    unsigned char qr :1; // query/response flag
+ 
+    unsigned char rcode :4; // response code
+    unsigned char cd :1; // checking disabled
+    unsigned char ad :1; // authenticated data
+    unsigned char z :1; // its z! reserved
+    unsigned char ra :1; // recursion available
+ 
+    unsigned short q_count; // number of question entries
+    unsigned short ans_count; // number of answer entries
+    unsigned short auth_count; // number of authority entries
+    unsigned short add_count; // number of resource entries
+};
+
+//Constant sized fields of query structure
+struct QUESTION
+{
+    unsigned short qtype;
+    unsigned short qclass;
+};
+ 
+//Constant sized fields of the resource record structure
+#pragma pack(push, 1)
+struct R_DATA
+{
+    unsigned short type;
+    unsigned short _class;
+    unsigned int ttl;
+    unsigned short data_len;
+};
+#pragma pack(pop)
+ 
+//Pointers to resource record contents
+struct RES_RECORD
+{
+    unsigned char *name;
+    struct R_DATA *resource;
+    unsigned char *rdata;
+};
+ 
+//Structure of a Query
+typedef struct
+{
+    unsigned char *name;
+    struct QUESTION *ques;
+} QUERY;
+
 
 //check IP dotted formatting.  
 //return 0: valid IP
 //return 1; invalid IP
 int validIPCheck(const char* ip)
-{   
-    int* splitedIP;
+{  
+    int splitedIP[4];
     int len = strlen(ip);
     char tail[16];
     int c, i; 
@@ -65,13 +125,20 @@ int main(int argc, char** argv)
 	 exit(2);
     }
      
-    validIPCheck(argv[1]);
- 
-    socket = openUDPSocket(argv[1], port);  
+    //check the invalid IP address formatting
+    returnVal = validIPCheck(argv[1]);
+    if(returnVal == 1)
+    {
+        printf("Error: Invalid IP address fromatting\n");
+	exit(2);
+    }
+
+    //socket = openUDPSocket(argv[1], port);  
     if(socket < 0)
         exit(2);
 
-    
+    //Now get the ip of hostname, A record
+    sendDNS(hostname, T_A);
 
   
     return 0;
