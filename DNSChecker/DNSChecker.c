cc5938689d489bf601b3d2f45ce28a8861f665a7
seongil-wi <su3604@kaist.ac.kr>
2017-05-16 19:22:03 +0900

    Check the IP address formatting

--- DNSChecker/DNSChecker.c
+++ DNSChecker/DNSChecker.c
@@ -9,16 +9,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//check IP dotted formatting.  
+//return 0: valid IP
+//return 1; invalid IP
+int validIPCheck(const char* ip)
+{   
+    int* splitedIP;
+    int len = strlen(ip);
+    char tail[16];
+    int c, i; 
+    tail[0] = 0;
+    
+    if(len < 7 || len > 15)
+        return 1;
+    
+    c = sscanf(ip, "%3u.%3u.%3u.%3u%s", &splitedIP[0], &splitedIP[1], &splitedIP[2], &splitedIP[3], tail);
+    
+    if(c != 4 || tail[0])
+        return 1;
+    
+    for(i = 0 ; i < 4; i ++)
+        if(splitedIP[i] > 255)
+            return 1;
+    
+    return 0;
+}
+
 int main(int argc, char** argv)
 {
     FILE *expect = NULL;
     unsigned short port = 0;
     char *endp;
+    int returnVal = 0;
+    int socket;
 
     if(argc != 3)
     {
         printf("Usage: ./DNSChecker <ip> <port>\n");
-	return 0;
+	exit(2);
     }
 
     expect = fopen("expect.csv", "r");
@@ -26,7 +54,7 @@ int main(int argc, char** argv)
     if(expect == NULL)
     {
         printf("Error: No \'expect.csv\' file\n");
-	return 0;
+	exit(2);
     }
 
     //convert string to unsigned short value 
@@ -34,8 +62,17 @@ int main(int argc, char** argv)
     if(endp == argv[2])
     {
          printf("Error: Can't convert the port string to unsigned short value\n");
-	 return 0;
+	 exit(2);
     }
+     
+    validIPCheck(argv[1]);
  
+    socket = openUDPSocket(argv[1], port);  
+    if(socket < 0)
+        exit(2);
+
+    
+
+  
     return 0;
 }
