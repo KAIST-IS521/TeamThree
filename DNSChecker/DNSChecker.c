ca32d985fbf30934955638d164802211254bf965
seongil-wi <su3604@kaist.ac.kr>
2017-05-16 16:39:28 +0900

    Add DNS SLA Checker folder

---
+++ DNSChecker/DNSChecker.c
@@ -0,0 +1,13 @@
+/*
+ * DNS SLA Checker 
+ *
+ * Usage: ./DNSChecker <ip> <port>
+ * Terminates with an exit code of 0: Operating normally
+ * Terminates with an exit code of 1: Operating abnormally
+ * Terminates with an exit code of 2: Cannot establish a connection to the target address
+ * */
+
+int main(char** argv, int argc)
+{
+    return 0;
+}
