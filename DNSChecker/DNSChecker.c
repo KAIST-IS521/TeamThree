fcf3a725319bf3f0ed7bfa9a6151e9ad3444f0d1
seongil-wi <su3604@kaist.ac.kr>
2017-05-16 17:16:00 +0900

    Add fild open system

--- DNSChecker/DNSChecker.c
+++ DNSChecker/DNSChecker.c
@@ -11,5 +11,21 @@
 
 int main(int argc, char** argv)
 {
+    FILE *expect = NULL;
+
+    if(argc != 3)
+    {
+        printf("Usage: ./DNSChecker <ip> <port>\n");
+	return 0;
+    }
+
+    expect = fopen("expect.csv", "r");
+
+    if(expect == NULL)
+    {
+        printf("Usage: ./DNSChecker <ip> <port>\n");
+	return 0;
+    }
+
     return 0;
 }
