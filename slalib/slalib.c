67c154fb29f74bd063c3507bfab5a566fd56cb5b
ByeongHak Lee <lbh0307@kaist.ac.kr>
2017-05-28 17:06:09 +0900

    style: Replace tabs to spaces

--- slalib/slalib.c
+++ slalib/slalib.c
@@ -18,17 +18,17 @@
 /*
 	gpgme error check func
 */
-#define fail_if_err(err)					\
-  do								\
-    {								\
-      if (err)							\
-        {							\
-          fprintf (stderr, "%s:%d: %s: %s\n",			\
-                   __FILE__, __LINE__, gpgme_strsource (err),	\
-		   gpgme_strerror (err));			\
-          exit (1);						\
-        }							\
-    }								\
+#define fail_if_err(err)                                      \
+  do                                                          \
+    {                                                         \
+      if (err)                                                \
+        {                                                     \
+          fprintf (stderr, "%s:%d: %s: %s\n",                 \
+                   __FILE__, __LINE__, gpgme_strsource (err), \
+          gpgme_strerror (err));                              \
+          exit (1);                                           \
+        }                                                     \
+    }                                                         \
   while (0)
 
 /*
